8dfdd36efcc8fa114d1ada4c939037263d056655
Victor <ebibari1@gmail.com>
2023-12-28 16:30:45 -0600

    parte 1 del curso de java script terminado

---
+++ JavaScript/ejercicio 1/JS/aplicacion/strings.js
@@ -0,0 +1,28 @@
+let nombre,
+    apellido,
+    nombre_Completo,
+    espacio;
+
+nombre = "juan";
+apellido = "perez";
+espacio = " ";
+
+//concatenacion de Strings
+
+nombre_Completo = nombre + apellido;
+
+console.log(nombre_Completo);
+
+console.log(nombre + apellido)
+
+console.log(nombre + " " + apellido)
+
+console.log('el significado de "DOOM" es document object model.')
+console.log("el significado de 'DOOM'  es document object model")
+
+console.log(`hola señor ${nombre} como se encuentra hoy? 
+            ¿es verdad que su nombre es ${nombre}?` )
+
+console.log(`hola ${"Sr."+apellido}`)
+
+ 
